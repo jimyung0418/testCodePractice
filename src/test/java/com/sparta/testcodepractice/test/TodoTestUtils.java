a44dcc083f33759ee35167d372f0d9954ca0572e
jimyung0418 <wjdwlaud0418@naver.com>
2023-12-04 18:58:46 +0900

    test : Controller test

---
+++ src/test/java/com/sparta/testcodepractice/test/TodoTestUtils.java
@@ -0,0 +1,31 @@
+package com.sparta.testcodepractice.test;
+
+import com.sparta.testcodepractice.todo.Todo;
+import com.sparta.testcodepractice.user.User;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.util.SerializationUtils;
+
+import java.time.LocalDateTime;
+
+public class TodoTestUtils {
+
+    public static Todo get(Todo todo, User user) {
+        return get(todo, 1L, LocalDateTime.now(), user);
+    }
+
+    /**
+     * 테스트용 할일 객체를 만들어주는 메서드
+     * @param todo 복제할 할일 객체
+     * @param id 설정할 아이디
+     * @param createDate 설정할 생성일시
+     * @param user 연관관계 유저
+     * @return 테스트용으로 생성된 할일 객체
+     */
+    public static Todo get(Todo todo, Long id, LocalDateTime createDate, User user) {
+        var newTodo = SerializationUtils.clone(todo);
+        ReflectionTestUtils.setField(newTodo, Todo.class, "id", id, Long.class);
+        ReflectionTestUtils.setField(newTodo, Todo.class, "createDate", createDate, LocalDateTime.class);
+        newTodo.setUser(user);
+        return newTodo;
+    }
+}
